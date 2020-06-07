f9837192037693fadf02c9e59f2b3a515d12a2e0
이경진 <bramhome5848@gmail.com>
2020-06-07 16:31:08 +0900

    20200607 qeurydsl function by spring data jpa

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -7,6 +7,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -20,7 +21,7 @@ import static org.springframework.util.StringUtils.isEmpty;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
-public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
+public class MemberRepositoryCustomImpl /*e xtends QuerydslRepositorySupport */ implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
@@ -28,8 +29,37 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
+//    public MemberRepositoryCustomImpl(Class<?> domainClass) {
+//        super(Member.class);
+//    }
+
     @Override
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+        /**
+         * getQuerydsl().applyPagination() 스프링 데이터가 제공하는 페이징을 Querydsl로 편리하게 변환 가능(단! Sort는 오류발생)
+         * from() 으로 시작 가능(최근에는 QueryFactory를 사용해서 select() 로 시작하는 것이 더 명시적) EntityManager 제공
+         * 한계
+         * Querydsl 3.x 버전을 대상으로 만듬
+         * Querydsl 4.x에 나온 JPAQueryFactory로 시작할 수 없음
+         * select로 시작할 수 없음 (from으로 시작해야함) QueryFactory 를 제공하지 않음
+         * 스프링 데이터 Sort 기능이 정상 동작하지 않음
+         */
+//        List<MemberTeamDto> result = from(member)
+//                .leftJoin(member.team, team)
+//                .where(
+//                        usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                )
+//                .select(new QMemberTeamDto(
+//                        member.id.as("memberId"),
+//                        member.username,
+//                        member.age,
+//                        team.id.as("teamId"),
+//                        team.name.as("teamName")))
+//                .fetch();
 
         return queryFactory
                 .select(new QMemberTeamDto(
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -108,5 +109,52 @@ class MemberRepositoryTest {
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
+
+    /**
+     * 인터페이스 지원 - QuerydslPredicateExecutor
+     * 조인X (묵시적 조인은 가능하지만 left join이 불가능.
+     * 클라이언트가 Querydsl에 의존해야 함
+     * 서비스 클래스가 Querydsl이라는 구현 기술에 의존해야 함.
+     * 복잡한 실무환경에서 사용하기에는 한계가 명확함.
+     */
+    @Test
+    public void querydslPredicateExecutorTest() {
+
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //when
+        QMember member = QMember.member;
+
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+
+        //then
+        for (Member findMember : result) {
+            System.out.println("member1 = " + findMember);
+        }
+    }
+
+    /**
+     * Querydsl Web 지원
+     * 단순한 조건만 가능
+     * 조건을 커스텀하는 기능이 복잡하고 명시적이지 않음 컨트롤러가 Querydsl에 의존
+     * 복잡한 실무환경에서 사용하기에는 한계가 명확
+     */
+    // -> 코드 생략
+
 
 }
